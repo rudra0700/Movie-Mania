3a03f644f8068f60da97a2716f56639564f9c1a3
rudra0700 <baruarudra7@gmail.com>
2024-10-08 18:02:22 +0600

    feat: review movie data sort by date

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ import { getMovieReviewsData } from "./data.js";
 const init = () => {
     const moviewReviewdata = getMovieReviewsData();
     
-    paintstatus(moviewReviewdata)
+    paintstatistic(moviewReviewdata);
+    // paintMovieData(moviewReviewdata);
+    sortDataByDate(moviewReviewdata);
+
 }
 
-const paintstatus = (moviewReviewdata) =>{
+const paintstatistic = (moviewReviewdata) =>{
     const flatReviewData = moviewReviewdata.flat();
     const totalMovies = moviewReviewdata.length;
     const totalReviews = flatReviewData.length;
@@ -34,7 +37,31 @@ const addStat = (element, value) =>{
     element.appendChild(span);
 } 
 
+const sortDataByDate = (moviewReviewdata) =>{
+   const flatReviewData = moviewReviewdata.flat();
+   flatReviewData.sort((a, b) => new Date(b.on) - new Date(a.on))
+
+   const movieListElement = document.querySelector("#movieListId ul");
+   movieListElement.innerHTML = "";
+   
+   flatReviewData.map((elem) =>{
+    const li = document.createElement("li");
+    li.classList.add("font-bold", "bg-gray-500", "p-4", "rounded-md", "text-white");
+    li.innerHTML = `
+             <p class="text-xl">${elem.title} - ${elem.rating}</p>
+                    <p>${elem.content}</p>
+                    <p>By ${elem.by} on ${new Intl.DateTimeFormat('en-BD').format(elem.on)}</p>
+    `
+    movieListElement.appendChild(li)
+   })
+   
+   
+}
+
+
 init()
+
+
 
 
 
